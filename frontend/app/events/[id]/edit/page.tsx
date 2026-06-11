ef9cd7da8c71c1b1fb0ac4b76ad42f0998c61063
gurubaksh yadav <gurubakshyadav71218@gmail.com>
2026-06-11 16:33:52 +0530

    Real event update functionality completed

--- frontend/app/events/[id]/edit/page.tsx
+++ frontend/app/events/[id]/edit/page.tsx
@@ -1,13 +1,40 @@
 import EditEventForm from "@/components/events/EditEventForm";
+import { supabase } from "@/lib/supabase";
 
-export default function EditEventPage() {
+export default async function EditEventPage({
+  params,
+}: {
+  params: Promise<{ id: string }>;
+}) {
+  const { id } = await params;
+
+  const { data: event } = await supabase
+    .from("events")
+    .select("*")
+    .eq("id", id)
+    .single();
+
+  if (!event) {
+    return (
+      <div>
+        Event Not Found
+      </div>
+    );
+  }
+
   return (
     <div>
+
       <h1 className="text-3xl font-bold">
         Edit Event
       </h1>
 
-      <EditEventForm />
+      <EditEventForm
+        id={event.id}
+        initialTitle={event.title}
+        initialTopic={event.topic}
+      />
+
     </div>
   );
 }
--- frontend/components/events/EditEventForm.tsx
+++ frontend/components/events/EditEventForm.tsx
@@ -1,23 +1,74 @@
 "use client";
 
-export default function EditEventForm() {
+import { useState } from "react";
+import { supabase } from "@/lib/supabase";
+import { useRouter } from "next/navigation";
+
+type Props = {
+  id: string;
+  initialTitle: string;
+  initialTopic: string;
+};
+
+export default function EditEventForm({
+  id,
+  initialTitle,
+  initialTopic,
+}: Props) {
+  const [title, setTitle] =
+    useState(initialTitle);
+
+  const [topic, setTopic] =
+    useState(initialTopic);
+
+  const router = useRouter();
+
+  const handleUpdate = async () => {
+    const { error } = await supabase
+      .from("events")
+      .update({
+        title,
+        topic,
+      })
+      .eq("id", id);
+
+    if (error) {
+      alert(error.message);
+      return;
+    }
+
+    alert("Event Updated");
+
+    router.push(`/events/${id}`);
+    router.refresh();
+  };
+
   return (
     <div className="bg-slate-900 p-6 rounded-xl mt-6">
+
       <input
-        placeholder="Title"
+        value={title}
+        onChange={(e) =>
+          setTitle(e.target.value)
+        }
         className="w-full p-3 rounded bg-slate-800 mb-3"
       />
 
       <input
-        placeholder="Topic"
+        value={topic}
+        onChange={(e) =>
+          setTopic(e.target.value)
+        }
         className="w-full p-3 rounded bg-slate-800 mb-3"
       />
 
       <button
+        onClick={handleUpdate}
         className="bg-green-600 px-4 py-2 rounded"
       >
         Save Changes
       </button>
+
     </div>
   );
 }
